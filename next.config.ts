9a011c9730bb66002c0dbac8f59ca396d591fa29
sajid1108 <cahsekolahbelajar@gmail.com>
2025-04-16 17:47:32 +0700

    fix(learn): styling, image component, and data fetch in nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://plus.unsplash.com/premium_photo-1682125139523-92d7def89cd1?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ),
+    ],
+  },
 };
 
 export default nextConfig;
