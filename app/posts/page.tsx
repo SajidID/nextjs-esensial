a0049b3f9e907f3edc073f2f261413c9ddd74715
sajid1108 <cahsekolahbelajar@gmail.com>
2025-04-17 12:21:12 +0700

    fix(data fetch): continue learn data fetch

---
+++ app/posts/page.tsx
@@ -0,0 +1,28 @@
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+async function getPosts(): Promise<Post[]> {
+const res = await fetch("http://localhost:3001/posts");
+const posts = await res.json();
+
+return posts
+
+}
+
+export default async function PostsPage() {
+  const posts = await getPosts();
+
+  return (
+    <div>
+      <h1>Posts Page</h1>
+      {posts.map((post) => (
+        <article key={post.id}>
+          <h2>{post.title}</h2>
+        </article>
+      ))}
+    </div>
+  );
+}
